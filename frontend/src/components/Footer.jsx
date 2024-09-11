b15c2ceda60dde517031c426c83304ff411d45c7
Prabhjot <prabhjotsinghrainu@gmail.com>
2024-09-11 22:34:41 +0530

    some changes

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,15 +5,15 @@ const Footer = () => {
 				<p className='text-balance text-center text-sm leading-loose text-muted-foreground md:text-left'>
 					Built by{" "}
 					<a
-						href='https://github.com/burakorkmez'
+						href='https://github.com/pr7bh'
 						target='_blank'
 						className='font-medium underline underline-offset-4'
 					>
-						you
+						Prabhjot Singh
 					</a>
 					. The source code is available on{" "}
 					<a
-						href='https://github.com/burakorkmez'
+						href='https://github.com/pr7bh'
 						target='_blank'
 						rel='noreferrer'
 						className='font-medium underline underline-offset-4'
